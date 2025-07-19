dd2bd287fa1bed5cae8b67db8974481ed05996ef
Aatika-Firdose <aatikafirdose9604@gmail.com>
2025-07-19 19:10:00 +0530

    ✅ Task 2 - Calculator Completed
    ✅ Task 2 - Calculator Completed

---
+++ calculator.py
@@ -0,0 +1,42 @@
+# Task 2 - Calculator
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero!"
+
+print("Welcome to  Python Calculator")
+print("Choose Operation:")
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
+
+choice = input("Enter choice (1/2/3/4): ")
+
+try:
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        print("Result:", add(num1, num2))
+    elif choice == '2':
+        print("Result:", subtract(num1, num2))
+    elif choice == '3':
+        print("Result:", multiply(num1, num2))
+    elif choice == '4':
+        print("Result:", divide(num1, num2))
+    else:
+        print("Invalid input")
+except ValueError:
+    print("Please enter valid numbers.")
